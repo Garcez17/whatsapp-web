e9b1c431b397f6aa9e404af5027346303665ea19
Gabriel Garcez <ggarcez613@gmail.com>
2024-11-03 23:26:10 -0300

    remove other sign in options

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,32 +39,11 @@ export default function Home() {
         <Content>
           <Info>
             <h1>To use WhatsApp on your computer:</h1>
-            <p>
-              1. Login social pq a API do WhatsApp é cara pra krl e o jeff bezos
-              já comeu minha carteira
-            </p>
           </Info>
           <ButtonsContainer>
-            <Button color="var(--white-100)">
-              <Image
-                src="/google_logo.png"
-                alt="WhatsApp"
-                width={24}
-                height={24}
-              />
-              Sign in with Google
-            </Button>
             <Button color="var(--black)" onClick={handleSingIn}>
               <AiFillGithub />
               Sign in with Github
-            </Button>
-            <Button color="linear-gradient(45deg, #405de6, #5851db, #833ab4, #c13584, #e1306c, #fd1d1d);">
-              <BsInstagram />
-              Sign in with Instagram
-            </Button>
-            <Button color="#738ABD">
-              <FaDiscord />
-              Sign in with Discord
             </Button>
           </ButtonsContainer>
         </Content>
